54c7b3b5bfcc5d682ea5e4d98bdf7ab86443f76a
Gonzalo Onisimchuk <gonzaonisimchuk@hotmail.com>
2017-08-29 18:12:51 -0300

    pequeños cambios y agrego logging

--- PoCFlask.py
+++ PoCFlask.py
@@ -1,69 +1,82 @@
 from flask import Flask, jsonify, make_response, request, abort
+import sys
+import logging as log
+import datetime as dt
 import RPi.GPIO as GPIO
 
+_host = '0.0.0.0'
+_port = 4010
+
+log.basicConfig(filename='{:%Y-%m-%d}.log'.format(dt.datetime.now()),
+				level=log.DEBUG, 
+				format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
 app = Flask(__name__)
-devices= {"Bano": 5,
-"P1": 13,
-"P2": 26,
-"P3": 19,
-"P4": 6,
-"Afuera": 21,
-"Test": 27 }
+devices = {
+	"PBa": 5,
+	"P1": 13,
+	"P2": 26,
+	"P3": 19,
+	"P4": 6,
+	"PAf": 21
+	}
 
 def setupPines():
-	GPIO.setup(6, GPIO.OUT)
-	GPIO.setup(13, GPIO.OUT)
-	GPIO.setup(19, GPIO.OUT)
-	GPIO.setup(21, GPIO.OUT)
-	GPIO.setup(26, GPIO.OUT)
-	GPIO.setup(27, GPIO.OUT)
+	try:
+		log.info("setupPines")
+		for device in devices.values():
+			log.info("GPIO.OUT: " + str(device))
+    		GPIO.setup(device, GPIO.OUT)
+	except:
+		log.exception(sys.exc_info()[0])
 
-def encender(pin):
+def encender(pin):	
 	GPIO.output(pin, True)
-	print ("encender pin:" + str(pin))
+	log.debug("encender pin:" + str(pin))
 	
 def apagar(pin):
 	GPIO.output(pin, False)
-	print ("apagar pin:" + str(pin))
+	log.debug("apagar pin:" + str(pin))
 
 @app.route('/',methods = ['GET'])
 def index():
-	if GPIO.input(27) == 0:
-		encender(27)
-	else:
-		apagar(27)
-	return "Hola"
+	return "Sitema funcionando"
 
 @app.route('/<id>',methods = ['GET'])
 def indexid(id):
 	pin = devices.get(id.upper())
 	if pin == None:
+		log.debug("Dispositivo no encontrado: " + id)
 		return "Dispositivo no encontrado"
 	if GPIO.input(pin) == 0:
 		encender(pin)
-		return "dispositivo " + id + " / pin " + str(pin) + " /encendido"
+		message = "{ dispositivo: " + id + ", pin: " + str(pin) + ", estado: encendido }"
+		log.info(message)
+		return message
 	else:
 		apagar(pin)
-		return "dispositivo " + id + " / pin " + str(pin) + " /apagado"
-#	return "Hola"
+		message = "{ dispositivo: " + id + ", pin: " + str(pin) + ", estado: apagado }"
+		log.info(message)
+		return message
 
 @app.route('/dispositivos',methods = ['GET'])
 def dispositivos():
+	log.debug("consulta dispositivos: " + str(devices))
 	return str(devices)
 	
-#capturadores de errores
-#@app.errorhandler(404)
-#def not_foud(error):
-#	return "error 404"
+# capturadores de errores
+@app.errorhandler(404)
+def not_foud(error):
+	return "error 404"
 
 	
-#@app.errorhandler(405)
-#def not_foud(error):
-#	return "error 405"
+@app.errorhandler(405)
+def not_foud(error):
+	return "error 405"
 
 if __name__ == '__main__':
-	print ("inicio")
+	app.run(debug = True, host=_host, port=_port)
+	log.info("Inicio")
 	GPIO.setwarnings(False)
 	GPIO.setmode(GPIO.BCM)
 	setupPines()
-	app.run(debug = True, host='0.0.0.0', port=80)
